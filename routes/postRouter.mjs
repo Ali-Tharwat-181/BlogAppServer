d2f4c65cd3bffc3bcbd5419d8cd02559d0b2f5c3
Ali-Tharwat-181 <alitharwat358@gmail.com>
2025-06-14 09:41:24 +0300

    .

--- routes/postRouter.mjs
+++ routes/postRouter.mjs
@@ -8,7 +8,27 @@ import {
 } from "../controllers/postConroller.mjs";
 import upload from "../utils/upload.mjs";
 import { isAuthenticated } from "../middlewares/isAuthenticatedMiddleware.mjs";
-import { isAuthorized } from "../middlewares/isAutherizedMiddleware.mjs";
+
+const allowCors = (handler) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://blog-app-roan-zeta.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Authorization, Content-Type"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await handler(req, res);
+};
 
 const router = express.Router();
 
@@ -23,4 +43,4 @@ router
   .put(upload.single("coverImage"), isAuthenticated, updatePost)
   .delete(isAuthenticated, deletePost);
 
-export default router;
+export default allowCors(router);
