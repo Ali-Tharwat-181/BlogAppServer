d57603af0b75bc9b72b2a8b4a85baa375660a96f
Ali-Tharwat-181 <alitharwat358@gmail.com>
2025-06-14 06:16:12 +0300

    added corsOptions

--- app.mjs
+++ app.mjs
@@ -10,12 +10,21 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+const corsOptions = {
+  origin: ["https://blog-app-roan-zeta.vercel.app/"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(morgan("dev"));
